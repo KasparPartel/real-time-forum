b1b11d36730edccc829ad1c39d72316c1b097797
Kaspar J. Pärtel <kasparjohannes@gmail.com>
2022-03-08 16:37:12 +0200

    responsive navbar nad basic layout done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Nav from "./Nav";
+import Feed from "./Feed";
 
 export default function App() {
   return (
-    <div>
+    <div className="flex">
       <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Feed />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,25 @@
 import { BrowserRouter, Link } from "react-router-dom";
-import "./nav.css";
+import { useState } from "react";
+
+import { HiPlus } from "react-icons/hi";
+import { BiEnvelope, BiLayer, BiLogOut, BiUser } from "react-icons/bi";
+import { MdOutlinePeopleAlt } from "react-icons/md";
 
 export default function Nav() {
+  const [active, setActive] = useState(false);
+
   return (
     <BrowserRouter>
-      <header className="nav__header">
-        <nav>
-          <ul>
+      {active ? (
+        <nav className="flex flex-col gap-20 sticky top-0 h-screen pl-4 pr-5 py-6 bg-red-100">
+          <HiPlus
+            size="1.6em"
+            className="self-end transition-transform duration-500 rotate-45 cursor-pointer"
+            onClick={() => setActive(false)}
+          />
+          <ul className="flex flex-col gap-3">
             <li className="nav__list-item">
-              <Link to="/feed">Feed</Link>
+              <Link to="/">Feed</Link>
             </li>
             <li className="nav__list-item">
               <Link to="/messages">Messages</Link>
@@ -16,9 +27,50 @@ export default function Nav() {
             <li className="nav__list-item">
               <Link to="/friends">Friends</Link>
             </li>
+            <li className="nav__list-item">
+              <Link to="/profile">Profile</Link>
+            </li>
           </ul>
+
+          <Link to="/logout" className="mt-auto">
+            Logout
+          </Link>
         </nav>
-      </header>
+      ) : (
+        <nav className="flex flex-col items-center gap-20 sticky top-0 h-screen px-4 py-6 bg-red-100">
+          <HiPlus
+            size="1.6em"
+            className="cursor-pointer"
+            onClick={() => setActive(true)}
+          />
+          <ul className="flex flex-col items-center gap-10">
+            <li className="nav__list-item">
+              <Link to="/">
+                <BiLayer size="1.4em" />
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              <Link to="/messages">
+                <BiEnvelope size="1.4em" />
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              <Link to="/friends">
+                <MdOutlinePeopleAlt size="1.4em" />
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              <Link to="/profile">
+                <BiUser size="1.4em" />
+              </Link>
+            </li>
+          </ul>
+
+          <Link to="/logout" className="mt-auto">
+            <BiLogOut size="1.4em" />
+          </Link>
+        </nav>
+      )}
     </BrowserRouter>
   );
 }
