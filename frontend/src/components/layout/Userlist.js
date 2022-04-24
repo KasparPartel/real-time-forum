bb6b62008ac27586121a0220d1cfd56a10dcf518
erkki.tikk <erkkitikk@gmail.com>
2022-04-24 22:23:28 +0300

    users showing from server

--- frontend/src/components/layout/Userlist.js
+++ frontend/src/components/layout/Userlist.js
@@ -1,12 +1,39 @@
-import classes from './Userlist.module.css';
+import React, { useEffect, useState } from "react";
+import classes from "./Userlist.module.css";
 
 function Userlist() {
-  
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    requestUsers();
+  }, []);
+
+  const requestUsers = () => {
+    fetch("http://localhost:4000/api/user/", {
+      method: "GET",
+      // headers: { "Content-Type": "application/json" },
+      // body: JSON.stringify(),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("okay");
+          return response.json();
+        }
+        throw new Error("Something went wrong");
+      })
+      .then((json) => setUsers(json))
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <ul className={classes.userlist}>
-        <li className={classes.online}>HC: Online user</li>
-        <li className={classes.offline}>HC: Offline user</li>
-    </ul>
+    <div className="flex flex-wrap py-5 gap-4">
+      <ul className={classes.userlist}>
+        {users.map((user) => (
+          //<Post key={p.id} json={p} />
+          <li className={classes.online}>{user.username}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
