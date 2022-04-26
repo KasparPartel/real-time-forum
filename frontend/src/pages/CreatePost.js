14c2ca3105a0ba05607086fa495b9342d364b035
erkki.tikk <erkkitikk@gmail.com>
2022-04-26 14:39:12 +0300

    added create post page

--- frontend/src/pages/CreatePost.js
+++ frontend/src/pages/CreatePost.js
@@ -1,9 +1,24 @@
-
-
 function CreatePost() {
- 
   return (
-    <div>CreatePost div</div> 
+    <div className="create-post">
+      <h1>Create a new post here:</h1>
+
+      <label>Post title: </label>
+      <input type="text" id="firstname" name="firstname" required />
+      <br></br>
+      <label>Post topic: </label>
+      <select name="post-topic" id="post-topic">
+        <option value="Memes">Memes</option>
+        <option value="Useful">Useful</option>
+        <option defaultValue="Random">Random</option>
+      </select>
+      <br></br>
+      <textarea id="post-body" name="post-body" rows="4" cols="50">
+        Enter your post here
+      </textarea>
+      <br></br>
+      <button>Submit</button>
+    </div>
   );
 }
 
